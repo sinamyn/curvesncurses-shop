c6ef3fc77fe691bbf57420624b84e518fcd213ed
sinamyn <sinamynbear@gmail.com>
2026-02-07 11:43:37 -0800

    Update env.ts

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,5 +1,5 @@
 export const env = {
-  siteName: process.env.NEXT_PUBLIC_SITE_NAME ?? "Curves~N~Curses by SinAmyn",
+  databaseUrl: process.env.DATABASE_URL ?? "",  siteName: process.env.NEXT_PUBLIC_SITE_NAME ?? "Curves~N~Curses by SinAmyn",
   tagline: process.env.NEXT_PUBLIC_TAGLINE ?? "Because basic was never your thing.",
   baseUrl: process.env.NEXT_PUBLIC_BASE_URL ?? "http://localhost:3000",
   authSecret: process.env.AUTH_SECRET ?? "change_me",
